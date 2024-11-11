92bcac934282ded1d61a7ccfa1421c7105dafab4
cbustoso <121638773+cbustoso@users.noreply.github.com>
2024-11-10 21:31:57 -0300

    update

--- api_sumate/database/development_db.py
+++ api_sumate/database/development_db.py
@@ -4,7 +4,7 @@ DB_MYSQL_DEVELOPMENT = {
         'NAME': 'sumate_v2',
         'USER': 'adminaz',
         'PASSWORD': 'BQ3q949zkTCL',
-        'HOST': 'bdsumatedev.mysql.database.azure.com',
+        'HOST': 'sumatedbs.mysql.database.azure.com',
         'PORT': '3306',
     }
 }
